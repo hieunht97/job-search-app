2ee1a367f7712f6f023f6235a0dcfe51254c03db
Hieu Tran <hieunht97@gmail.com>
2024-11-26 19:24:29 -0500

    minor fix

--- frontend/src/pages/User/register/Register.jsx
+++ frontend/src/pages/User/register/Register.jsx
@@ -1,13 +1,13 @@
-import React, { useState} from "react"
+import React, { useState } from "react";
 import axios from "axios";
-
+import { Link } from "react-router-dom";
 
 const Register = () => {
-  const [email, setEmail] = useState("")
-  const [pw, setPw] = useState("")
-  const [confPw, setConfPw] = useState("")
-  const [err, setErr] = useState("")
-  
+  const [email, setEmail] = useState("");
+  const [pw, setPw] = useState("");
+  const [confPw, setConfPw] = useState("");
+  const [err, setErr] = useState("");
+
   // validate email format
   const validateEmail = (email) => {
     const regex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/;
@@ -15,35 +15,35 @@ const Register = () => {
   };
 
   const handleClick = async (event) => {
-    event.preventDefault()
+    event.preventDefault();
 
     // validate email and password
     if (!validateEmail(email)) {
-      setErr("Invalid email")
-      console.log("Invalid email")
-      return
+      setErr("Invalid email");
+      console.log("Invalid email");
+      return;
     }
 
     if (pw != confPw) {
-      setErr("Password does not match")
-      console.log("Password does not match")
-      return
+      setErr("Password does not match");
+      console.log("Password does not match");
+      return;
     }
 
     // post to backend
     try {
-      const response = await axios.post("http://localhost:3000/auth/register", {email: email, password: pw})
-      console.log("Registered successfully:")
-      console.log(response.data)
-      location.replace("http://localhost:5173/login")
-      
-    } catch(err) {
-      console.log(err)
+      const response = await axios.post("http://localhost:3000/auth/register", {
+        email: email,
+        password: pw,
+      });
+      console.log("Registered successfully:");
+      console.log(response.data);
+      location.replace("http://localhost:5173/login");
+    } catch (err) {
+      console.log(err);
     }
-    setErr("")
-    
-  }
-
+    setErr("");
+  };
 
   return (
     <>
@@ -51,39 +51,43 @@ const Register = () => {
         <div className="shape"></div>
         <div className="shape"></div>
       </div>
-      <form>
+      <form className="register">
         <h3>Register</h3>
 
         <label htmlFor="email">Email</label>
-        <input 
-          value={email} 
-          onChange={(event) => setEmail(event.target.value)} 
-          type="email" 
-          placeholder="JohnDoe@gmail.com" 
+        <input
+          value={email}
+          onChange={(event) => setEmail(event.target.value)}
+          type="email"
+          placeholder="JohnDoe@gmail.com"
           id="email"
         />
 
         <label htmlFor="pw">Password</label>
-        <input 
-          value={pw} 
-          onChange={(event) => setPw(event.target.value)} 
-          type="password" 
-          id = "pw"
+        <input
+          value={pw}
+          onChange={(event) => setPw(event.target.value)}
+          type="password"
+          id="pw"
         />
 
         <label htmlFor="confPw">Confirm Password</label>
-        <input 
-          value={confPw} 
-          onChange={(event) => setConfPw(event.target.value)} 
-          type="password" 
-          id = "confPw"
+        <input
+          value={confPw}
+          onChange={(event) => setConfPw(event.target.value)}
+          type="password"
+          id="confPw"
         />
 
         {err && <p style={{ color: "red" }}>{err}</p>}
 
-        <button onClick={handleClick} type="submit">Register</button>
+        <button onClick={handleClick} type="submit" className="register">
+          Register
+        </button>
         <div className="social">
-          <h4> LOGIN </h4>
+          <div className="login">
+            <Link to="/login"> LOGIN </Link>
+          </div>
         </div>
       </form>
     </>
